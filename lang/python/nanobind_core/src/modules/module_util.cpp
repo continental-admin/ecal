fb83aa9513b1509ac989fc1a21bc29a08d2885ec
Kerstin Keller <KerstinKeller@users.noreply.github.com>
2024-10-09 14:04:53 +0200

    Changes due to rebasing.

--- lang/python/nanobind_core/src/modules/module_util.cpp
+++ lang/python/nanobind_core/src/modules/module_util.cpp
@@ -53,7 +53,7 @@ void AddUtilFuncToModule(nanobind::module_& module)
     module.def("get_topic_datatype_info", [](const std::string& nb_topic_name_)
         {
             eCAL::SDataTypeInformation nb_topic_info_;
-            auto success = eCAL::Util::GetTopicDataTypeInformation(nb_topic_name_, nb_topic_info_);
+            auto success = eCAL::Registration::GetTopicDataTypeInformation(nb_topic_name_, nb_topic_info_);
             auto return_value = nanobind::make_tuple(success, nb_topic_info_);
             return return_value;
         });
@@ -65,9 +65,9 @@ void AddUtilFuncToModule(nanobind::module_& module)
     //module.def("get_service_names", [](std::vector<std::tuple<std::string, std::string>>& nb_service_method_names_)
     //    { return eCAL::Util::GetServiceNames(nb_service_method_names_); });
     module.def("get_service_type_names", [](const std::string& nb_service_name_, const std::string& nb_method_name_, std::string& nb_req_type_, std::string& nb_resp_type_)
-        { return eCAL::Util::GetServiceTypeNames(nb_service_name_, nb_method_name_, nb_req_type_, nb_resp_type_); });
+        { return eCAL::Registration::GetServiceTypeNames(nb_service_name_, nb_method_name_, nb_req_type_, nb_resp_type_); });
     module.def("get_service_description", [](const std::string& nb_service_name_, const std::string& nb_method_name_, std::string& nb_req_desc_, std::string& nb_resp_desc_)
-        { return eCAL::Util::GetServiceDescription(nb_service_name_, nb_method_name_, nb_req_desc_, nb_resp_desc_); });
+        { return eCAL::Registration::GetServiceDescription(nb_service_name_, nb_method_name_, nb_req_desc_, nb_resp_desc_); });
 
     module.def("split_combined_topic_type", [](const std::string& nb_combined_topic_type_)
         { return eCAL::Util::SplitCombinedTopicType(nb_combined_topic_type_); });
